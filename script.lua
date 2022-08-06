aa7456ff0d3b9205730f88ab3234086618646fd9
Lu Kai <lukai@mail.ustc.edu.cn>
2022-08-07 01:11:01 +0800

    [update] script.lua memory monitor

--- script.lua
+++ script.lua
@@ -2,5 +2,6 @@ local count = 0
 while true do
     count = count + 1
     print('[lua] triggered by lua vm thread :', count)
+    print('[lua] consumed memory :', collectgarbage('count'))
     coroutine.yield()
 end
